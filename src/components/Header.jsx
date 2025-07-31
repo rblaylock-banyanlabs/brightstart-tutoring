1f14063bcb54c699053fb234a2cfbc2ad83c3e55
Annie Baysinger <abaysinger@proton.me>
2025-07-31 13:43:01 -0700

    Changes links to home, our tutors, and contacts. Moves div with links over to the right. Changes font weight to normal for links. Removes hover on links for mobile devices. Removes focus ring over collapsible button on mobile devices. Removes dark mode classes.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,21 +8,20 @@ const Header = () => {
 
   return (
     <>
-      <nav className="font-inter bg-yellow-500 dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b-2 border-black dark:border-gray-600">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <nav className="font-inter bg-yellow-500 fixed w-full z-20 top-0 start-0">
+        <div className="max-w-screen-lg flex flex-wrap justify-between mx-auto p-4">
           <a
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <LuGraduationCap size={32} />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            <span className="self-center text-2xl font-semibold whitespace-nowrap">
               Bright Start Tutoring
             </span>
           </a>
-          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             <button
               data-collapse-toggle="navbar-sticky"
               type="button"
-              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-black rounded-lg md:hidden"
               aria-controls="navbar-sticky"
               aria-expanded={menuOpen}
                 onClick={() => setMenuOpen((prev => !prev))} //Toggle menu state
@@ -44,56 +43,37 @@ const Header = () => {
                 />
               </svg>
             </button>
-          </div>
           <div
             className={`items-center justify-between w-full md:flex md:w-auto md:order-1" ${menuOpen ? "flex" : "hidden"}`}
             id="navbar-sticky"
           >
-            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul className="flex flex-col p-4 md:p-0 mt-4 font-normal rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
               <li>
                 <a
-                  href="#hero"
-                  id="hero"
-                  className="block py-2 px-3 rounded-sm md:bg-transparent md:p-0 md:dark:text-blue-500"
+                  href="#home"
+                  id="home"
+                  className="block py-2 px-3 rounded-sm md:bg-transparent md:p-0"
                   aria-current="page"
                 >
-                  Hero
+                  Home
                 </a>
               </li>
               <li>
                 <a
-                  href="#services"
-                  id="services"
-                  className="block py-2 px-3 text-black rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  href="#tutors"
+                  id="tutors"
+                  className="block py-2 px-3 text-black font-normal rounded-sm md:p-0"
                 >
-                  Services
+                  Our Tutors
                 </a>
               </li>
               <li>
                 <a
-                  href="#testimonials"
-                  id="testimonials"
-                  className="block py-2 px-3 text-black rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  href="#contacts"
+                  id="contacts"
+                  className="block py-2 px-3 text-black font-normal rounded-sm md:p-0"
                 >
-                  Testimonials
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#contact"
-                  id="contact"
-                  className="block py-2 px-3 text-black rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Contact
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#footer"
-                  id="footer"
-                  className="block py-2 px-3 text-black rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Footer
+                  Contacts
                 </a>
               </li>
             </ul>
